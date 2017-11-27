c1a61f68c281087859d91685833e250732ac3fcf
MBKeeler <mark.keeler@comcast.net>
2017-11-27 13:41:15 -0500

    Adds code to enable creation of a user
    Adds code to enable the creation of a user with the new field of
    accnt_type.
    
    Adds a scrips examples file.
    
    Adds an example curl script for sign-up of a user with accnt_type of
    'coach'.

---
+++ scripts/examples.sh
@@ -0,0 +1,5 @@
+# Sign-up
+EMAIL="mark123" PASSWORD="day" PASSWORD="day" ACCOUNT_TYPE="coach" sh scripts/auth/sign-up.sh
+
+# Sign-in
+EMAIL="mark123" PASSWORD="day" PASSWORD="day" sh scripts/auth/sign-in.sh
