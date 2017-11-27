324cca4bb8149e9d428ff31b72ab0a9d46b14291
MBKeeler <mark.keeler@comcast.net>
2017-11-27 14:08:26 -0500

    Adds curl script example for admin
    Adds a curl script example for user sign-up with accnt_type of admin

---
+++ scripts/examples.sh
@@ -0,0 +1,7 @@
+# Sign-up - coach
+EMAIL="mark123" PASSWORD="day" PASSWORD="day" ACCOUNT_TYPE="coach" sh scripts/auth/sign-up.sh
+# Sign-up - admibn
+EMAIL="mark" PASSWORD="day" PASSWORD="day" ACCOUNT_TYPE="admin" sh scripts/auth/sign-up.sh
+
+# Sign-in
+EMAIL="mark123" PASSWORD="day" PASSWORD="day" sh scripts/auth/sign-in.sh
